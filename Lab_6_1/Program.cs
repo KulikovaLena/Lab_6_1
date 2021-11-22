55cbe36a52a51b47b869358d7dad32e05a5e98b3
11111 <11111@DESKTOP-I2P1BG6>
2021-11-22 14:54:48 +0300

    Add project files.

---
+++ Lab_6_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string Long = " ";
+            int a = 1;
+            foreach (string s in stringArray)
+            {
+                if (s.Length>a)
+                {
+                    a = s.Length;
+                    Long = s;
+                }
+            }
+            Console.WriteLine(Long);
+            Console.ReadKey();
+        }
+    }
+}
